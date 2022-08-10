4a6265682ec10d037ccc30f324f717303c9bbd94
Sujitra <sujitra3304@hotmail.com>
2022-08-09 19:32:57 -0700

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod,)
+while True:
+    user_input = input(">  ")
+    if input == "q":
+        break
+
+# Replace this with your code
