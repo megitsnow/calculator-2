5451fcf47056e48b363303cd6d4fae53163e71a9
Sujitra <sujitra3304@hotmail.com>
2022-08-09 20:35:59 -0700

    Added division,square, and cube functions

--- calculator.py
+++ calculator.py
@@ -4,18 +4,30 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod,)
 while True:
     input_string = input(">  ")
-    if len(input_string)>4:
+    if input_string == 'q':
+        print("Exit")
+        break
+    elif len(input_string) < 3:
+         print ("Not enough input")
+    
+    else:
         calc_input = input_string.split(' ')
-        operator = calc_input[0]
-        num1 = calc_input[1]
-        num2 = calc_input[2]
-        num1 = float(num1)
-        num2 = float(num2)
+        
+        if len(calc_input)==3:
+            operator = calc_input[0]
+            num1 = calc_input[1]
+            num2 = calc_input[2]
+            num1 = float(num1)
+            num2 = float(num2)
+        if len(calc_input)==2:
+            operator = calc_input[0]
+            num1 = calc_input[1]
+            num1 = float(num1)
 
         if operator == "+":
             print(add(num1,num2))
 
-        elif operator == "-":  
+        elif operator == "-":
             print(subtract(num1, num2))
 
         elif operator == "*":  
@@ -23,10 +35,15 @@ while True:
         
         elif operator == "/":
             print(divide(num1, num2))
+
+        elif operator == "**":
+            print(square(num1))
+
+        elif operator =="cube":
+             print(cube(num1))
+
             
-    elif input_string == "q":
-        break
-    else:
-        print("please enter an operator and 2 numbers")
+    # elif input_string == "q":
+    #     break
 
 # Replace this with your code
